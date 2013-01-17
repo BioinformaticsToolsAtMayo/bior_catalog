07a3b32efe58a90b564d24f9f8e43a3c586d58a2
mrm4408 <mrm4408@8f329c14-d232-4f17-8684-7ee34322b8dc>
2013-01-17 17:08:06 +0000

    Switched to use new injector code.

--- src/main/java/edu/mayo/bior/publishers/HapMap/HapMapPublisherPhase2.java
+++ src/main/java/edu/mayo/bior/publishers/HapMap/HapMapPublisherPhase2.java
@@ -4,20 +4,25 @@
  */
 package edu.mayo.bior.publishers.HapMap;
 
+import java.io.IOException;
+import java.util.Arrays;
+
 import com.tinkerpop.pipes.util.Pipeline;
-import edu.mayo.pipes.AppendStringPipe;
+
+import edu.mayo.pipes.PrintPipe;
 import edu.mayo.pipes.JSON.DrillPipe;
 import edu.mayo.pipes.JSON.InjectIntoJsonPipe;
-import edu.mayo.pipes.JSON.tabix.SameVariantPipe;
-import edu.mayo.pipes.PrependStringPipe;
-import edu.mayo.pipes.PrintPipe;
+import edu.mayo.pipes.JSON.inject.ColumnArrayInjector;
+import edu.mayo.pipes.JSON.inject.ColumnInjector;
+import edu.mayo.pipes.JSON.inject.Injector;
+import edu.mayo.pipes.JSON.inject.JsonType;
+import edu.mayo.pipes.JSON.inject.LiteralInjector;
 import edu.mayo.pipes.UNIX.CatPipe;
 import edu.mayo.pipes.bioinformatics.vocab.CoreAttributes;
 import edu.mayo.pipes.bioinformatics.vocab.Type;
-import edu.mayo.pipes.history.*;
-import java.io.IOException;
-import java.util.AbstractMap;
-import java.util.Arrays;
+import edu.mayo.pipes.history.FindAndReplaceHPipe;
+import edu.mayo.pipes.history.HCutPipe;
+import edu.mayo.pipes.history.HistoryInPipe;
 
 /**
  *
@@ -49,15 +54,19 @@ public class HapMapPublisherPhase2 {
         int[] col = new int[] {2,3,6,8};
         DrillPipe drill = new DrillPipe(true, paths);
         FindAndReplaceHPipe replaceChr = new FindAndReplaceHPipe(1, "chr", "");
-        InjectIntoJsonPipe inject = new InjectIntoJsonPipe(8, new AbstractMap.SimpleEntry(CoreAttributes._type.toString(), Type.VARIANT.toString()), 
-                                                              new AbstractMap.SimpleEntry("1",CoreAttributes._landmark.toString()), 
-                                                              new AbstractMap.SimpleEntry("2", CoreAttributes._minBP.toString()),
-                                                              new AbstractMap.SimpleEntry("3", CoreAttributes._maxBP.toString()),
-                                                              new AbstractMap.SimpleEntry("4", CoreAttributes._strand.toString()),
-                                                              new AbstractMap.SimpleEntry("5", CoreAttributes._refAllele.toString()),
-                                                              new AbstractMap.SimpleEntry("6", CoreAttributes._altAlleles.toString()),
-                                                              new AbstractMap.SimpleEntry("7", CoreAttributes._id.toString())
-                    );
+        Injector[] injectors = new Injector[]
+        		{
+        			new LiteralInjector(CoreAttributes._type.toString(), Type.VARIANT.toString(), JsonType.STRING),
+        			new ColumnInjector     (1, CoreAttributes._landmark.toString(),   JsonType.STRING),
+        			new ColumnInjector     (2, CoreAttributes._minBP.toString(),      JsonType.NUMBER),
+        			new ColumnInjector     (3, CoreAttributes._maxBP.toString(),      JsonType.NUMBER),
+        			new ColumnInjector     (4, CoreAttributes._strand.toString(),     JsonType.STRING),
+        			new ColumnInjector     (5, CoreAttributes._refAllele.toString(),  JsonType.STRING),
+        			new ColumnArrayInjector(6, CoreAttributes._altAlleles.toString(), JsonType.STRING, ","),
+        			new ColumnInjector     (7, CoreAttributes._id.toString(),         JsonType.STRING)        			        			        			
+        		};
+        
+        InjectIntoJsonPipe inject = new InjectIntoJsonPipe(8, injectors);                    
         int[] cut2 = new int[] {1,2,3,4,5,6,7};
         Pipeline p = new Pipeline(new CatPipe(), 
                                   new HistoryInPipe(), 
