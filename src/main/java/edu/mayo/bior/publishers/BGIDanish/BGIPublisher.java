d76ab3072b544c788c4cda4de53912c1a32a537e
m102417 <m102417@8f329c14-d232-4f17-8684-7ee34322b8dc>
2013-01-18 15:35:43 +0000

    adding cosmic raw data format

--- src/main/java/edu/mayo/bior/publishers/BGIDanish/BGIPublisher.java
+++ src/main/java/edu/mayo/bior/publishers/BGIDanish/BGIPublisher.java
@@ -22,6 +22,7 @@ import edu.mayo.pipes.PrintPipe;
 import edu.mayo.pipes.UNIX.CatGZPipe;
 import edu.mayo.pipes.UNIX.CatPipe;
 import edu.mayo.pipes.UNIX.LSPipe;
+import edu.mayo.pipes.WritePipe;
 import edu.mayo.pipes.bioinformatics.vocab.CoreAttributes;
 import edu.mayo.pipes.bioinformatics.vocab.Type;
 import edu.mayo.pipes.history.HCutPipe;
@@ -45,14 +46,14 @@ public class BGIPublisher {
     
     public static void main(String[] args) {	 
         BGIPublisher publisher = new BGIPublisher();
-        publisher.publish("/data/BGI/hg19/LuCAMP_200exomeFinal_hg19.txt", "/tmp", new PrintPipe());
+        //publisher.publish("/data/BGI/hg19/LuCAMP_200exomeFinal_hg19.txt", "/tmp", new PrintPipe());
 //        System.out.println(args.length);
-//        if(args.length >= 1){ 
-//            publisher.publish(args[1], args[2] + "/scratch/");
-//        }else{
-//            usage();
-//            System.exit(1);
-//        }
+        if(args.length >= 1){ 
+            publisher.publish(args[1], args[2] + "/scratch/", new WritePipe("/tmp/LuCAMP_200exomeFinal_hg19.txt"));
+        }else{
+            usage();
+            System.exit(1);
+        }
     } 
     
     public void publish(String rawDataFileFullpath, String outputDir, Pipe out) {
@@ -82,34 +83,34 @@ public class BGIPublisher {
         Injector[] injectors = new Injector[] {
             new ColumnInjector(1, "chromosomeID", JsonType.STRING),
             new ColumnInjector(2, "genomic_position", JsonType.STRING),
-            new ColumnInjector(3, "index_of_major_allele", JsonType.STRING),
-            new ColumnInjector(4, "index_of_minor_allele", JsonType.STRING),
-            new ColumnInjector(5, "number_A", JsonType.STRING),
-            new ColumnInjector(6, "number_C", JsonType.STRING), 
-            new ColumnInjector(7, "number_G", JsonType.STRING),
-            new ColumnInjector(8, "number_T", JsonType.STRING),
+            new ColumnInjector(3, "index_of_major_allele", JsonType.NUMBER),
+            new ColumnInjector(4, "index_of_minor_allele", JsonType.NUMBER),
+            new ColumnInjector(5, "number_A", JsonType.NUMBER),
+            new ColumnInjector(6, "number_C", JsonType.NUMBER), 
+            new ColumnInjector(7, "number_G", JsonType.NUMBER),
+            new ColumnInjector(8, "number_T", JsonType.NUMBER),
             new ColumnInjector(9, "estimatedMAF", JsonType.STRING),
             new ColumnInjector(10, CoreAttributes._type.toString(), JsonType.STRING),
             new ColumnInjector(11, CoreAttributes._landmark.toString(), JsonType.STRING),
             new ColumnInjector(12, CoreAttributes._refAllele.toString(), JsonType.STRING),
             new ColumnArrayInjector(13, CoreAttributes._altAlleles.toString(), JsonType.STRING, ","),
-            new ColumnInjector(14, CoreAttributes._minBP.toString(), JsonType.STRING),
-            new ColumnInjector(15, CoreAttributes._maxBP.toString(), JsonType.STRING)       			        			        			
+            new ColumnInjector(14, CoreAttributes._minBP.toString(), JsonType.NUMBER),
+            new ColumnInjector(15, CoreAttributes._maxBP.toString(), JsonType.NUMBER)       			        			        			
         };
-        InjectIntoJsonPipe inject = new InjectIntoJsonPipe(injectors);
+        InjectIntoJsonPipe inject = new InjectIntoJsonPipe(true, injectors);
         int[] cut = new int[] {1,2,3,4,5,6,7,8,9,10,12,13};
         int[] cut2 = new int[] {1};
         Pipe p = new Pipeline(new CatPipe(),
                              new HistoryInPipe(),
                              t,
                              inject,
-                             //new HCutPipe(false, cut), 
+                             new HCutPipe(false, cut), 
                              new MergePipe("\t"),
                              out);
         p.setStarts(Arrays.asList(rawDataFileFullpath));
         for(int i=0;p.hasNext();i++){
             p.next();
-            if(i>100) break;
+            //if(i>100) break;
         }
     }
     
