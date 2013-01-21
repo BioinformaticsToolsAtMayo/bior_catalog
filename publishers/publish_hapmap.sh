3f96ef62b5fcf38b90ceaecbee0b06eddac1a401
m054457 <m054457@8f329c14-d232-4f17-8684-7ee34322b8dc>
2013-01-21 16:29:27 +0000

    Slight update to hapmap publisher script, though it may still not work yet

--- publishers/publish_hapmap.sh
+++ publishers/publish_hapmap.sh
@@ -43,16 +43,23 @@ fi
 
 echo 'Running HapMap Publisher Phase 1/6...';
 #java -cp $BIOR_CATALOG_HOME/conf:$BIOR_CATALOG_HOME/lib/* edu.mayo.bior.publishers.HapMap.HapMapPublisher $0 $@ 
+
 echo 'Converting hapmap.tsv to sorted gff, Phase 2/6...';
 #cat ${outDir}/scratch/hapmap.tsv | bior_drill.sh -k -p chrom -p source -p type -p pos -p pos -p score -p strand -p phase | grep -v ^# | sort -k1,1 -k4,4n > ${outDir}/scratch/hapmap.sorted.tsv
+
 echo 'Performing liftOver, Phase 3/6...'
 $UCSC_TOOLS_HOME/liftOver -gff ${outDir}/scratch/hapmap.sorted.tsv  $UCSC_TOOLS_HOME/hg18toHg19.over.chain ${outDir}/scratch/hapmap.sorted.liftover.tsv ${outDir}/scratch/unmapped.tsv
+
 echo 'Merging the records, Phase 4/6...'
+java -cp $BIOR_CATALOG_HOME/conf:$BIOR_CATALOG_HOME/lib/* edu.mayo.bior.publishers.HapMap.HapMapPublisherPhase2 ${outDir}/scratch/hapmap.sorted.liftover.tsv  ${outDir}/scratch/hapmap.collapsedJson.tsv
+
+echo 'Do we need to sort here before compressing and building tabix?????'
 
 echo 'Compressing the catalog, Phase 5/6...'
+java -cp $BIOR_CATALOG_HOME/conf:$BIOR_CATALOG_HOME/lib/* edu.mayo.bior.indexer.cmd.BuildBgzip ${outDir}/scratch/hapmap.collapsedJson.tsv  ${outDir}/hapmap.tsv.bgz
 
 echo 'Building Tabix Index, Phase 6/6...'
 ### Build Tabix index
-#tabix -s 1 -b 2  $outDir/dbsnp.tsv.bgz
+tabix -s 1 -b 2  $outDir/hapmap.tsv.bgz
 
 echo "Done."
