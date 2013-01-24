fe513ff10703d84d0d9e3a51cf7e5753d5d45c20
m054457 <m054457@8f329c14-d232-4f17-8684-7ee34322b8dc>
2013-01-24 06:05:32 +0000

    Needed to sort after doing the liftover

--- publishers/publish_hapmap.sh
+++ publishers/publish_hapmap.sh
@@ -41,25 +41,26 @@ if [ -z "$UCSC_TOOLS_HOME" ] && [ "${UCSC_TOOLS_HOME+x}" = "x" ]; then
 fi
 ##e.g. /data/ucsc/exe/macOSX.i386
 
-echo 'Running HapMap Publisher Phase 1/6...';
-java -cp $BIOR_CATALOG_HOME/conf:$BIOR_CATALOG_HOME/lib/* edu.mayo.bior.publishers.HapMap.HapMapPublisher $hapmapDir $outDir/scratch/hapmap.tsv 
+echo 'Running HapMap Publisher - Step 1/7...';
+java -cp $BIOR_CATALOG_HOME/conf:$BIOR_CATALOG_HOME/lib/* edu.mayo.bior.publishers.HapMap.HapMapPublisher $hapmapDir $outDir/scratch/hapmap.1.tsv 
 
-echo 'Converting hapmap.tsv to sorted gff, Phase 2/6...';
-cat ${outDir}/scratch/hapmap.tsv | bior_drill -k -p chrom -p source -p type -p pos -p pos -p score -p strand -p phase | grep -v ^# | sort -k1,1 -k4,4n > ${outDir}/scratch/hapmap.sorted.tsv
+echo 'Converting hapmap.tsv to sorted gff - Step 2/7...';
+cat ${outDir}/scratch/hapmap.1.tsv | bior_drill -k -p chrom -p source -p type -p pos -p pos -p score -p strand -p phase | grep -v ^# | sort -k1,1 -k4,4n > ${outDir}/scratch/hapmap.2.sorted.tsv
 
-echo 'Performing liftOver, Phase 3/6...'
-$UCSC_TOOLS_HOME/liftOver -gff ${outDir}/scratch/hapmap.sorted.tsv  $UCSC_TOOLS_HOME/hg18toHg19.over.chain ${outDir}/scratch/hapmap.sorted.liftover.tsv ${outDir}/scratch/unmapped.tsv
+echo 'Performing liftOver - Step 3/7...'
+echo 'NOTE: After liftover, the records may not necessarily be sorted anymore'
+$UCSC_TOOLS_HOME/liftOver -gff ${outDir}/scratch/hapmap.2.sorted.tsv  $UCSC_TOOLS_HOME/hg18toHg19.over.chain ${outDir}/scratch/hapmap.3.liftover.tsv ${outDir}/scratch/hapmap.3.unmapped.tsv
 
-echo 'Merging the records, Phase 4/6...'
-java -cp $BIOR_CATALOG_HOME/conf:$BIOR_CATALOG_HOME/lib/* edu.mayo.bior.publishers.HapMap.HapMapPublisherPhase2 ${outDir}/scratch/hapmap.sorted.liftover.tsv  ${outDir}/scratch/hapmap.collapsedJson.tsv
+echo 'Sorting the records again after liftover - Step 4/7'
+sort -k1,1 -k4,4n -k5,5n ${outDir}/scratch/hapmap.3.liftover.tsv  >  ${outDir}/scratch/hapmap.4.liftover.sorted.tsv 
 
-echo 'Do we need to sort here before compressing and building tabix?????'
+echo 'Merging the records - Step 5/7...'
+java -cp $BIOR_CATALOG_HOME/conf:$BIOR_CATALOG_HOME/lib/* edu.mayo.bior.publishers.HapMap.HapMapPublisherPhase2 ${outDir}/scratch/hapmap.4.liftover.sorted.tsv  ${outDir}/scratch/hapmap.5.collapsedJson.tsv
 
-echo 'Compressing the catalog, Phase 5/6...'
-bgzip -c ${outDir}/scratch/hapmap.collapsedJson.tsv > ${outDir}/allele_freqs.grc37.tsv.bgz
-### java -cp $BIOR_CATALOG_HOME/conf:$BIOR_CATALOG_HOME/lib/* edu.mayo.bior.indexer.cmd.BuildBgzip ${outDir}/scratch/hapmap.collapsedJson.tsv  ${outDir}/allele_freqs.tsv.bgz
+echo 'Compressing the catalog - Step 6/7...'
+bgzip -c ${outDir}/scratch/hapmap.5.collapsedJson.tsv > ${outDir}/allele_freqs.grc37.tsv.bgz
 
-echo 'Building Tabix Index, Phase 6/6...'
+echo 'Building Tabix Index - Step 7/7...'
 ### Build Tabix index
 tabix -s 1 -b 2 -e 3 $outDir/allele_freqs.grc37.tsv.bgz
 
