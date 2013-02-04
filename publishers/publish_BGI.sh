fae61b3b152e1553dfac3938f776caa7f83c8e5f
m054457 <m054457@8f329c14-d232-4f17-8684-7ee34322b8dc>
2013-02-04 22:24:29 +0000

    Added columns to the post liftover files so we can compare the minBP of the maf vs genotype files (in the BGIPublisher) to ensure that the correct lines were matched up in the paste/combine command

--- publishers/publish_BGI.sh
+++ publishers/publish_BGI.sh
@@ -22,17 +22,17 @@ zcat $rawDir/LuCAMP_200exomeFinal.maf.gz | sort -k1,1 -k2,2n  > $scratch/1.maf.s
 ### add minBP on the end so we have the original hg18 data (liftover will modify the first few position columns)
 ### keep only the refAllele and isDbSNP columns from genotype file
 ### Format:
-###  chr  .  .  minBP  maxBP  majorAlleleIndex  minorAlleleIndex  ACount  CCount  GCount  TCount  alleleFreq  minBP  refAllele  isInDbSNP
-echo -e "###chr\t.\t.\tminBP\tmaxBP\tmajIdx\tminIdx\tAs\tCs\tGs\tTs\tFreqs\tminBpOrig\tRefAllele\tisInDbSNP"  >  $scratch/2.mafPlusRef.tsv
+###  chr  .  .  minBP  maxBP  majorAlleleIndex  minorAlleleIndex  ACount  CCount  GCount  TCount  alleleFreq  minBpPreLiftoverMaf minBpPreLiftoverGenotype  refAllele  isInDbSNP
+echo -e "###chr\t.\t.\tminBP\tmaxBP\tmajIdx\tminIdx\tAs\tCs\tGs\tTs\tAlleleFreqs\tminBpPreLiftoverMaf\tminBpPreLiftoverGenotype\tRefAllele\tisInDbSNP"  >  $scratch/2.mafPlusRef.tsv
 paste  $scratch/1.maf.sorted.tsv  $scratch/1.genotype.part.sorted.tsv  \
-  | awk -F \\t '{print $1 FS "." FS "." FS $2 FS $2 FS $3 FS $4 FS $5 FS $6 FS $7 FS $8 FS "\""$9"\"" FS $11 FS $12 FS $13}'  \
+  | awk -F \\t '{print $1 FS "." FS "." FS $2 FS $2 FS $3 FS $4 FS $5 FS $6 FS $7 FS $8 FS "\""$9"\"" FS $2 FS $11 FS $12 FS $13}'  \
   >>  $scratch/2.mafPlusRef.tsv
 
 ### Liftover the BGI minBP & maxBP hg18 to hg19
 $liftOverDir/liftOver -gff  $scratch/2.mafPlusRef.tsv  $liftOverDir/hg18ToHg19.over.chain  $scratch/3.mafPlusRef.liftedOver.tsv  $scratch/3.unmapped.txt
 
-### Re-sort the file
-sort -k1,1 -k4,4 $scratch/3.mafPlusRef.liftedOver.tsv > $scratch/4.mafPlusRef.liftedOver.sorted.tsv
+### Re-sort the file, but with the minBP treated as a numeric column
+sort -k1,1 -k4,4n $scratch/3.mafPlusRef.liftedOver.tsv > $scratch/4.mafPlusRef.liftedOver.sorted.tsv
 
 ### Call BGIPublisher to convert the majorIdx/minIdx to base-pairs, and to create the JSON column
 java -cp $BIOR_CATALOG_HOME/conf:$BIOR_CATALOG_HOME/lib/* edu.mayo.bior.publishers.BGIDanish.BGIPublisher $scratch/4.mafPlusRef.liftedOver.sorted.tsv $scratch/5.catalog.json.tsv
