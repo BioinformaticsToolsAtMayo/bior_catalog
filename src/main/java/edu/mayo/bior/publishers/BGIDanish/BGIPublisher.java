297b614a7154320cf1244a224473338114ced730
m054457 <m054457@8f329c14-d232-4f17-8684-7ee34322b8dc>
2013-02-04 20:23:22 +0000

    Changed ref alleles to pick it up from the genotype file instead of using the major allele from maf. Also calculating alt alleles based on counts of non-zero alleles that are also not the ref allele

--- src/main/java/edu/mayo/bior/publishers/BGIDanish/BGIPublisher.java
+++ src/main/java/edu/mayo/bior/publishers/BGIDanish/BGIPublisher.java
@@ -1,145 +1,128 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.mayo.bior.publishers.BGIDanish;
+
+import java.sql.Timestamp;
+import java.util.Arrays;
+import java.util.Date;
 
 import com.tinkerpop.pipes.Pipe;
 import com.tinkerpop.pipes.PipeFunction;
 import com.tinkerpop.pipes.transform.TransformFunctionPipe;
 import com.tinkerpop.pipes.util.Pipeline;
-import edu.mayo.bior.publishers.Cosmic.CosmicPublisher;
-import edu.mayo.pipes.AppendStringPipe;
-import edu.mayo.pipes.HeaderPipe;
+
+import edu.mayo.pipes.MergePipe;
+import edu.mayo.pipes.PrintPipe;
+import edu.mayo.pipes.WritePipe;
 import edu.mayo.pipes.JSON.InjectIntoJsonPipe;
 import edu.mayo.pipes.JSON.inject.ColumnArrayInjector;
 import edu.mayo.pipes.JSON.inject.ColumnInjector;
 import edu.mayo.pipes.JSON.inject.Injector;
 import edu.mayo.pipes.JSON.inject.JsonType;
 import edu.mayo.pipes.JSON.inject.LiteralInjector;
-import edu.mayo.pipes.MergePipe;
-import edu.mayo.pipes.PrependStringPipe;
-import edu.mayo.pipes.PrintPipe;
-import edu.mayo.pipes.UNIX.CatGZPipe;
 import edu.mayo.pipes.UNIX.CatPipe;
-import edu.mayo.pipes.UNIX.LSPipe;
-import edu.mayo.pipes.WritePipe;
 import edu.mayo.pipes.bioinformatics.vocab.CoreAttributes;
 import edu.mayo.pipes.bioinformatics.vocab.Type;
-import edu.mayo.pipes.bioinformatics.vocab.Undefined;
 import edu.mayo.pipes.history.HCutPipe;
 import edu.mayo.pipes.history.History;
 import edu.mayo.pipes.history.HistoryInPipe;
-import edu.mayo.pipes.history.HistoryOutPipe;
 import edu.mayo.pipes.util.GenomicObjectUtils;
-import java.sql.Timestamp;
-import java.util.AbstractMap;
-import java.util.Arrays;
-import java.util.Date;
 
 /**
- *
  * @author m102417
  */
 public class BGIPublisher {
     public static void usage(){
         System.out.println("usage: BGIPublisher <rawDataFile> <rawOutputFile>");
+        System.out.println("<rawDataFile> should be of the following format:");
+        System.out.println("###chr	.	.	minBP	maxBP	majIdx	minIdx	As	Cs	Gs	Ts	Freqs	minBpOrig	RefAllele	isInDbSNP");
+        System.out.println("chr1	.	.	69428	69428	3	2	0	7	86	1871	\"0.073021\"	59291	T	0");
     }
+    
     
     public static void main(String[] args) {	 
         BGIPublisher publisher = new BGIPublisher();
-        //Pipe out = new Pipeline(new AppendStringPipe("\n"), new WritePipe("/tmp/LuCAMP_200exomeFinal_hg19.tsv"));
-        //publisher.publish("/data/BGI/hg19/LuCAMP_200exomeFinal_hg19.txt", out);
-//        System.out.println(args.length);
-        final String catalogFile = "LuCAMP_200exomeFinal.tsv";   
-        if(args.length >= 2){ 
-            String outfile = args[2];
-            String infile = args[1];
-            System.out.println("Input File: " + infile);
-            System.out.println("Outputing File to: " + outfile);  
-            Pipe out = new Pipeline(new AppendStringPipe("\n"), new WritePipe(outfile));
-            publisher.publish(infile, out);
-            
-        }else{
+        // Ex:  publisher.publish("/data/BGI/hg19/LuCAMP_200exomeFinal_hg19.txt", "/tmp/LuCAMP_200exomeFinal_hg19.tsv");
+        if( args.length != 2 ) {
             usage();
             System.exit(1);
         }
+
+        String infile = args[0];
+        String outfile = args[1];
+        System.out.println("Input File:  " + infile);
+        System.out.println("Output File: " + outfile);  
+        publisher.publish(infile, outfile);
     } 
     
-    public void publish(String rawDataFileFullpath, Pipe out) {
+    public void publish(String rawDataFileFullpath, String outfile) {
         double start = System.currentTimeMillis();
         System.out.println("Started loading BGI at: " + new Timestamp(new Date().getTime()));
-        Pipe<History,History> t = new TransformFunctionPipe<History,History>( new BGIPipe() );
-        String[] header = new String[] {
-                                        "chromosomeID",
-                                        "genomic_position",
-                                        "index_of_major_allele",
-                                        "index_of_minor_allele",
-                                        "number_A",
-                                        "number_C",
-                                        "number_G",
-                                        "number_T",
-                                        "estimatedMAF",
-                                        CoreAttributes._type.toString(),
-                                        CoreAttributes._landmark.toString(),
-                                        //CoreAttributes._refAllele.toString(),
-                                        CoreAttributes._altAlleles.toString(),
-                                        CoreAttributes._minBP.toString(),
-                                        CoreAttributes._maxBP.toString()
-                                            };
-        Injector[] injectors = new Injector[] {
+        Pipe<History,History> xformPipe = new TransformFunctionPipe<History,History>( new BGIPipe() );
+        InjectIntoJsonPipe inject = new InjectIntoJsonPipe(true, new Injector[] {
             new ColumnInjector(1, "chromosomeID", JsonType.STRING),
-            new ColumnInjector(2, "genomic_position", JsonType.STRING),
-            new ColumnInjector(3, "index_of_major_allele", JsonType.NUMBER),
-            new ColumnInjector(4, "index_of_minor_allele", JsonType.NUMBER),
-            new ColumnInjector(5, "number_A", JsonType.NUMBER),
-            new ColumnInjector(6, "number_C", JsonType.NUMBER), 
-            new ColumnInjector(7, "number_G", JsonType.NUMBER),
-            new ColumnInjector(8, "number_T", JsonType.NUMBER),
-            new ColumnInjector(9, "estimatedMAF", JsonType.STRING),
-            new ColumnInjector(10, CoreAttributes._type.toString(), JsonType.STRING),
-            new ColumnInjector(11, CoreAttributes._landmark.toString(), JsonType.STRING),
-            new ColumnInjector(12, CoreAttributes._refAllele.toString(), JsonType.STRING),
-            new ColumnArrayInjector(13, CoreAttributes._altAlleles.toString(), JsonType.STRING, ","),
-            new ColumnInjector(14, CoreAttributes._minBP.toString(), JsonType.NUMBER),
-            new ColumnInjector(15, CoreAttributes._maxBP.toString(), JsonType.NUMBER),
+            new ColumnInjector(4, "genomic_position", JsonType.STRING),
+            new ColumnInjector(13,"genomic_position_original", JsonType.STRING),
+            new ColumnInjector(6, "index_of_major_allele", JsonType.NUMBER),
+            new ColumnInjector(7, "index_of_minor_allele", JsonType.NUMBER),
+            new ColumnInjector(8, "number_A", JsonType.NUMBER),
+            new ColumnInjector(9, "number_C", JsonType.NUMBER), 
+            new ColumnInjector(10,"number_G", JsonType.NUMBER),
+            new ColumnInjector(11,"number_T", JsonType.NUMBER),
+            new ColumnInjector(12, "estimatedMAF", JsonType.STRING),
+            new ColumnInjector(19, "isInDbSNP", JsonType.STRING),
+            new ColumnInjector(17, CoreAttributes._landmark.toString(), JsonType.STRING),
+            new ColumnInjector(15, CoreAttributes._refAllele.toString(), JsonType.STRING),
+            new ColumnArrayInjector(18, CoreAttributes._altAlleles.toString(), JsonType.STRING, ","),
+            //some checking on hg19 shows it to be one based not zero based
+            new ColumnInjector(4, CoreAttributes._minBP.toString(), JsonType.NUMBER),
+            new ColumnInjector(5, CoreAttributes._maxBP.toString(), JsonType.NUMBER),
+        	new LiteralInjector(CoreAttributes._type.toString(), Type.VARIANT.toString(), JsonType.STRING),
             new LiteralInjector(CoreAttributes._id.toString(),".",JsonType.STRING)
-        };
-        InjectIntoJsonPipe inject = new InjectIntoJsonPipe(true, injectors);
-        int[] cut = new int[] {1,2,3,4,5,6,7,8,9,10,12,13};
-        int[] cut2 = new int[] {1};
+        });
+
         Pipe p = new Pipeline(new CatPipe(),
                              new HistoryInPipe(),
-                             t,
+                             xformPipe,
                              inject,
-                             new HCutPipe(false, cut), 
+                             new HCutPipe(false, new int[] {2,3,6,7,8,9,10,11,12,13,14,15,16,17,18,19} ), 
                              new MergePipe("\t"),
+                         	 // Write to file: Don't append to file;  Add newlines to each line
+                             new WritePipe(outfile, false, true),
                              //new PrintPipe()
-                             out
                 );
         p.setStarts(Arrays.asList(rawDataFileFullpath));
-        for(int i=0;p.hasNext();i++){
+        while(p.hasNext()) {
             p.next();
-            //if(i>100) break;
         }
+        double end = System.currentTimeMillis();
+        System.out.println("Total runtime: " + (end-start)/1000.0 + " sec");
     }
     
     public class BGIPipe implements PipeFunction<History,History>{
+        @Override
+        /** Add on extra columns that we will then inject into the JSON object */
+        public History compute(History h) {
+            //_landmark
+            h.add(  GenomicObjectUtils.computechr(h.get(0) ));
+            
+            //_altAllele
+            String ref = h.get(14);
+            String altsJson = getAltAllelesJson(ref, Integer.parseInt(h.get(7)), Integer.parseInt(h.get(8)), Integer.parseInt(h.get(9)), Integer.parseInt(h.get(10)));
+            h.add(altsJson);
+            
+            // isInDbSNP - convert to true/false (instead of 0/1)
+            String isInDbSNP0or1 = h.get(15);
+            h.add("" + "1".equals(isInDbSNP0or1));
+            
+            // Throw an exception if the minBP column from the original maf file (col 12) 
+            // does NOT match the minBP column from the original genotype file (col 13)
+            if( ! h.get(12).equals(h.get(13)) )
+            	throw new IllegalArgumentException("The minBP from the maf does NOT match the minBP from the genotype file!");
+            
+            return h;
+        }
 
-        /*
-         * col1: chromosomeID
-           col2: genomic position
-           col3: index_of_major_allele
-           col4: index_of_minor_allele
-           col5: # of A
-           col6: # of C
-           col7: # of G
-           col8: # of T
-           col9: estimatedMAF
-
-           indexs 0, 1, 2, 3 correspond to A, C, G, T.
-         */
-        private String decode(String s){
+    	/** Convert index to base-pair:  0=A, 1=C, 2=G, 3=T */
+    	private String decode(String s){
             if(s.endsWith("0")){
                 return "A";
             }else if(s.endsWith("1")){
@@ -152,23 +135,27 @@ public class BGIPublisher {
             return null;
         }
 
-        
-        @Override
-        public History compute(History h) {
-            //_type
-            h.add(Type.VARIANT.toString());
-            //_landmark
-            h.add(  GenomicObjectUtils.computechr(h.get(0) ));
-            //_minBP
-            h.add(decode(h.get(2)));
-            //_maxBP
-            h.add(decode(h.get(3)));
-            //some checking on hg19 shows it to be one based not zero based
-            h.add((h.get(1)));
-            h.add((h.get(1)));
-            return h;
+        /** Get the alt alleles as a comma-separated string.  
+         *  Check the # of As, Cs, Gs, Ts - if any are non-zero AND are NOT the ref allele, add them to list */ 
+        private String getAltAllelesJson(String refAllele, int numAs, int numCs, int numGs, int numTs) {
+        	String alts = "";
+        	
+        	if( ! "A".equals(refAllele) && numAs > 0 )
+        		alts += "A,";
+        	
+        	if( ! "C".equals(refAllele) && numCs > 0 )
+        		alts += "C,";
+        	
+        	if( ! "G".equals(refAllele) && numGs > 0 )
+        		alts += "G,";
+
+        	if( ! "T".equals(refAllele) && numTs > 0 )
+        		alts += "T";
+        		
+        	if(alts.endsWith(","))
+        		alts = alts.substring(0,alts.length()-1);
+        	return  alts;
         }
-        
     }
     
 }
