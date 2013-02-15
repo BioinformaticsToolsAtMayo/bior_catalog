22c6c25185e670d056e7048f5dcc8af08d50f9be
m054457 <m054457@8f329c14-d232-4f17-8684-7ee34322b8dc>
2013-02-15 02:05:00 +0000

    adding test data for ucsc publisher

---
+++ src/test/resources/testData/ucsc/switchDbTss.sql
@@ -0,0 +1,50 @@
+-- MySQL dump 10.11
+--
+-- Host: localhost    Database: hg19
+-- ------------------------------------------------------
+-- Server version	5.0.67
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `switchDbTss`
+--
+
+DROP TABLE IF EXISTS `switchDbTss`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `switchDbTss` (
+  `bin` smallint(5) unsigned NOT NULL,
+  `chrom` varchar(255) NOT NULL,
+  `chromStart` int(10) unsigned NOT NULL,
+  `chromEnd` int(10) unsigned NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `score` int(10) unsigned NOT NULL,
+  `strand` char(1) NOT NULL,
+  `confScore` double NOT NULL,
+  `gmName` varchar(255) NOT NULL,
+  `gmChromStart` int(10) unsigned NOT NULL,
+  `gmChromEnd` int(10) unsigned NOT NULL,
+  `isPseudo` tinyint(3) unsigned NOT NULL,
+  KEY `chrom` (`chrom`(8),`bin`),
+  KEY `chromStart` (`chrom`(8),`chromStart`),
+  KEY `name` (`name`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+SET character_set_client = @saved_cs_client;
+
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2011-01-18  9:23:48
