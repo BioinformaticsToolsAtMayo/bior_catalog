4aeeec8af6edc5a50727c04c21e4d3e516667dd7
m089716 <m089716@8f329c14-d232-4f17-8684-7ee34322b8dc>
2013-03-04 16:25:24 +0000

    new script

---
+++ publishers/publish_hgnc.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#------------------------------------------------------------------------------------------
+# Assumptions:
+# 1) sys.properties will be loaded and the properties contain the correct path to the chr.gbs.txt files
+# 2) Tabix is installed on the local machine
+# 3) BIOR_CATALOG_HOME variable has been set
+#------------------------------------------------------------------------------------------
+# Print each line that is executed (-x), and exit if any command fails (-e)
+#set -x -e
+set -e
+#echo $BIOR_CATALOG_HOME
+#source $BIOR_CATALOG_HOME/setupEnv.sh
+
+
+#------------------------------------------------------------------------------------------
+# Given a raw data files (*.gbs.txt) build JSON data file (which contains position + JSON)
+#------------------------------------------------------------------------------------------
+rawDataDir=$1
+targetCatalogDir=$2
+
+echo "Build JSON from raw data files"
+echo "Target directory: $targetCatalogDir"
+java -cp $BIOR_CATALOG_HOME/conf:$BIOR_CATALOG_HOME/lib/* edu.mayo.bior.publishers.HUGO.HUGOPublisher $rawDataDir $targetCatalogDir
+
+echo "DONE."
