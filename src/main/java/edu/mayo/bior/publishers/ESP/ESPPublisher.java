89e3e76abe22cfff3f925708332a6af842ad09b5
m089716 <m089716@8f329c14-d232-4f17-8684-7ee34322b8dc>
2013-05-02 17:01:01 +0000

    Publisher to publish ESP catalog

--- src/main/java/edu/mayo/bior/publishers/ESP/ESPPublisher.java
+++ src/main/java/edu/mayo/bior/publishers/ESP/ESPPublisher.java
@@ -1,181 +1,220 @@
 package edu.mayo.bior.publishers.ESP;
 
-import java.awt.List;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.sql.Timestamp;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonPrimitive;
 import com.tinkerpop.pipes.Pipe;
 import com.tinkerpop.pipes.PipeFunction;
 import com.tinkerpop.pipes.transform.TransformFunctionPipe;
 import com.tinkerpop.pipes.util.Pipeline;
 
-import edu.mayo.bior.publishers.Cosmic.CosmicPublisher.CosmicTransformPipe;
 import edu.mayo.pipes.MergePipe;
-import edu.mayo.pipes.PrependStringPipe;
-import edu.mayo.pipes.PrintPipe;
 import edu.mayo.pipes.WritePipe;
-import edu.mayo.pipes.JSON.InjectIntoJsonPipe;
-import edu.mayo.pipes.JSON.inject.ColumnArrayInjector;
-import edu.mayo.pipes.JSON.inject.ColumnInjector;
-import edu.mayo.pipes.JSON.inject.Injector;
-import edu.mayo.pipes.JSON.inject.JsonType;
-import edu.mayo.pipes.JSON.inject.LiteralInjector;
+import edu.mayo.pipes.JSON.DrillPipe;
 import edu.mayo.pipes.UNIX.CatPipe;
-import edu.mayo.pipes.bioinformatics.vocab.CoreAttributes;
-import edu.mayo.pipes.bioinformatics.vocab.Type;
+import edu.mayo.pipes.UNIX.GrepPipe;
+import edu.mayo.pipes.UNIX.LSPipe;
+import edu.mayo.pipes.bioinformatics.VCF2VariantPipe;
 import edu.mayo.pipes.history.HCutPipe;
 import edu.mayo.pipes.history.History;
 import edu.mayo.pipes.history.HistoryInPipe;
 
 /**
  * 
- * @author m089716
+ * @author m089716 Surendra Konathala
  *
  */
 public class ESPPublisher {
+	private static Logger sLogger = Logger.getLogger(ESPPublisher.class);
 	
 	public static void usage(){
-        System.out.println("usage: ESPPublisher <rawDataFile> <catalogOutputDir>");
+        System.out.println("usage: ESPPublisher <rawDataDir> <ESPTsvOutfile>");
     }
 	
 	public static void main(String[] args) {	 
-		ESPPublisher publisher = new ESPPublisher();      
-        /*
-        if(args.length >= 1){ 
-            publisher.exec(args[0], args[1] + "/scratch/");
-        }else{
-            usage();
-            System.exit(1);
-        }
-        */
-		publisher.publish("/Users/m089716/data/esp/ESP6500SI.chr21.snps_indels.vcf", "Users/m089716/data/tmp/");
+		try {
+			ESPPublisher publisher = new ESPPublisher();      
+	        
+	        if(args.length >= 1){ 
+	            publisher.publish(args[0], args[1]);
+	        }else{
+	            usage();
+	            System.exit(1);
+	        }
+			
+			//publisher.publish("/Users/m089716/data/esp", "/Users/m089716/data/tmp/ESP6500SI_GRCh37.tsv");			
+		}catch(Exception e) {
+    		e.printStackTrace();
+    	}        
     }   
 	
-	public void processESPFile() {
-		
-	}
-
+	
 	/** 
 	 * 
 	 * @param filename - input raw data file
 	 * @param outputDir - the directory where the catalog file is written
 	 */
-	public void publish(String filename, String outputDir) {
-		final String catalogFile = "ESP6500SI_GRCh37.tsv";
-    	
-        System.out.println("Started loading ESP at: " + new Timestamp(new Date().getTime()));
-        
-        //String outfile = outputDir + "\\" + catalogFile; 
-        String outfile = outputDir + catalogFile;        
-        System.out.println("Outputing File to: " + outfile);
+	public void publish(String rawDataDir, String outfile) throws FileNotFoundException {
 		
-        Injector[] injectors = new Injector[15];
+		verifyInputDirAndOutputFile(rawDataDir, outfile);
     	
-    	//for(int i=0;i<headerColumns.size();i++) {    		
-    	//	injectors[i] =  new ColumnInjector(i+1, headerColumns.get(i), JsonType.STRING);
-    	//}
-       	//Add CoreAttributes    	
-        injectors[0] = new ColumnInjector(1, "CHROM", JsonType.STRING);
-        injectors[1] = new ColumnInjector(2, "POS", JsonType.STRING);
-        injectors[2] = new ColumnInjector(3, "ID", JsonType.STRING);
-        injectors[3] = new ColumnInjector(4, "REF", JsonType.STRING);
-        injectors[4] = new ColumnInjector(5, "ALT", JsonType.STRING);
-        injectors[5] = new ColumnInjector(6, "QUAL", JsonType.STRING);
-        injectors[6] = new ColumnInjector(7, "FILTER", JsonType.STRING);
-        injectors[7] = new ColumnInjector(11, "INFO", JsonType.STRING); 
-        injectors[8] = new ColumnInjector(9, CoreAttributes._type.toString(), JsonType.STRING);                
-    	injectors[9] = new ColumnInjector(1, CoreAttributes._landmark.toString(), JsonType.STRING);
-    	injectors[10] = new ColumnInjector(4, CoreAttributes._refAllele.toString(), JsonType.STRING);
-    	injectors[11] = new ColumnArrayInjector(5, CoreAttributes._altAlleles.toString(), JsonType.STRING, ",");
-    	injectors[12] = new ColumnInjector(2, CoreAttributes._minBP.toString(), JsonType.NUMBER);
-    	injectors[13] = new ColumnInjector(10, CoreAttributes._maxBP.toString(), JsonType.NUMBER);
-    	//injectors[31] = new ColumnInjector(32, CoreAttributes._strand.toString(), JsonType.STRING);
-    	injectors[14] = new ColumnInjector(3, CoreAttributes._id.toString(),JsonType.STRING);
-    	
-        InjectIntoJsonPipe injectCosmicDataAsJson = new InjectIntoJsonPipe(true, injectors);
-        
-        // The final catalog file will have "landmark<tab>minbp<tab>maxbp<tab>JSON string"
-         // Writing only columns 27: landmark/chr; 30: minbp; 31: maxbp to the final catalog file. 
-        // The JSON string with all values from the raw file will be appended to the above. 
-        int[] cut = new int[] {3,4,5,6,7,8,9,10,11};
+    	double start = System.currentTimeMillis();
+    	System.out.println("Started loading ESP at: " + new Timestamp(new Date().getTime()));
+        System.out.println("Outputing File to: " + outfile);
 
+        try {
+            System.out.println("Parsing ESP from: " + rawDataDir); //chrDir);            
+            
+            Pipeline p = new Pipeline(new LSPipe(false), new GrepPipe("ESP6500SI.chr2*.*"));
+            p.setStarts(Arrays.asList(new String[] {rawDataDir}));
+            for(int i = 0; p.hasNext(); i++){ 
+                String filename = (String)p.next();
+                System.out.println("Processing File: " + filename);
+
+                processESPFile(rawDataDir + "/" + filename,                        
+                        new WritePipe(outfile)
+                        //new PrintPipe()
+                        );
+            }
+        } catch (Exception ex) {
+            sLogger.error("Failed to publish ESP catalog!", ex);
+            ex.printStackTrace();
+        }        
+        System.out.println("Completed loading ESP at: " + new Timestamp(new Date().getTime()));
+        double end = System.currentTimeMillis();
+        System.out.println("Runtime: " + (end-start)/1000.0);
+	}
+	
+	/** */
+	public void processESPFile(String filename, Pipe load) {
+		
+        int[] cut = new int[] {3,4,5,6,7,8,10,11};
+        
+        String[] paths = {"_maxBP", "INFO.MAF"};             
+        
         Pipe<History,History> transform = new TransformFunctionPipe<History,History>( new ESPTransformPipe() );
         
-        WritePipe writePipe = new WritePipe(outfile);
-		
 		Pipe pipeline = new Pipeline(new CatPipe(),
-                new HistoryInPipe(),
-                transform,                
-                injectCosmicDataAsJson,
-                new HCutPipe(false, cut),
-                new MergePipe("\t", true),
-                new PrintPipe());
+				new HistoryInPipe(),
+				new VCF2VariantPipe(),				
+				new DrillPipe(true, paths),				
+				transform,
+				new HCutPipe(false, cut),
+				new MergePipe("\t", true), 
+				load
+				);
 		pipeline.setStarts(Arrays.asList(filename));
 		
 		for(int i=0; pipeline.hasNext(); i++){
 			pipeline.next();
-			if (i > 3) break;				
+			//if (i > 1) break;				
 		}
 	}
 	
-	/**
-	 * 
-	 * @author m089716
-	 *
-	 */
+	/** Verify that the rawDataDir exists and is a directory */
+	private void verifyInputDirAndOutputFile(String rawDataDir, String outfile) throws FileNotFoundException {
+    	if( ! new File(rawDataDir).exists() || ! new File(rawDataDir).isDirectory() ) {
+    		String msg = "Input directory does not exist or is not a directory: " + rawDataDir;
+    		System.err.println(msg);
+    		throw new FileNotFoundException(msg);
+    	}
+    	
+    	// outfile should NOT be a directory
+    	if(new File(outfile).exists() && new File(outfile).isDirectory())
+    		throw new FileNotFoundException("Output file must be a file, not a directory: " + outfile);
+    	
+    	// outfile's parent directory must exist
+    	File outputDir = new File(outfile).getParentFile();
+    	if(! outputDir.exists())
+    		throw new FileNotFoundException("The parent directory for the output file does not exist: " + outputDir);
+    	
+        // Delete the file if it already exists so we start with an empty file
+    	if(new File(outfile).exists())
+    		new File(outfile).delete();
+    }
+	
+	/**	Transforms/splits the RAW MAF data in INFO column from ["0.02","0.213","0.003"] 
+	 * to "EA":{"_maf":[0.0026]},"AA":{"_maf":[0.0094]},"ALL":{"_maf":[0.002]}} 
+	*/
 	public class ESPTransformPipe implements PipeFunction<History,History> {
+		public String MAF_EA = "";
+		public String MAF_AA = "";
+		public String MAF_ALL = "";
+		
+		final String[] MAF_LITERALS = { "EA", "AA", "ALL"};	
 		
 		@Override
-		public History compute(History history) {
-			
-			//_type
-            history.add(Type.VARIANT.toString());
-        
-            //maxBp
-            history.add(history.get(1)); //TODO how to get the maxbp??
-            
-            //Processed INFO
-            history.add(processedInfoColumn(history.get(7)));
-            
+		public History compute(History history) {			
+			//Processed INFO
+			history.add(processInfo(history.get(9), history.get(10)));
+						
 			return history;
-		}
-		
-		private String processedInfoColumn(String infoColumnData) {
-			
-			String processedInfo= ""; 
-			
-			if (infoColumnData.equals(null) || infoColumnData.equals("")) {
-				processedInfo = ".";
-			} else {
-				String raw = infoColumnData;
-				String[] rawValues = infoColumnData.split(";");
-				
-				Map<String, String> mapValues = new HashMap<String, String>();
-				
-				for(int i=0;i<rawValues.length;i++) {
-					//System.out.println(rawValues[i]);
-					String key = rawValues[i].substring(0,rawValues[i].indexOf("="));
-					String value = rawValues[i].substring( rawValues[i].indexOf("="), rawValues[i].length());
-					System.out.println(key +"::"+ value);
+		}		
 
-					mapValues.put(key, value);
-					
-				}
+		/**
+		 * 
+		 * @param rawMAFLine - ["0.02","0.213","0.003"]
+		 * @param infoLine - {"CHROM":"12",.....}
+		 * @return - infoLine with added MAF values as {"CHROM":"12"....,"EA":{"_maf":[0.0026]},"AA":{"_maf":[0.0094]},"ALL":{"_maf":[0.002]}}
+		 */
+		private String processInfo(String rawMAFLine, String infoLine) {
+			
+		    JsonParser jparser = new JsonParser();
+		    JsonObject root = jparser.parse(infoLine).getAsJsonObject();
+		    	
+		    JsonObject proot = this.processMAF(rawMAFLine, root);
+		
+		    return proot.toString();
+		}
+
+		/** */
+		private JsonObject processMAF(String rawMAFLine, JsonObject proot) {			
+			if (rawMAFLine.equals(null) || rawMAFLine.equals("")) {
+				//processedInfo = ".";
+			} else {
+				String trimRawMAFLine = rawMAFLine.replaceAll("\\[|\\]", ""); //remove [ ]
+				trimRawMAFLine = trimRawMAFLine.replaceAll("\"", ""); // remove "
 				
-				//System.out.println("RAW="+Arrays.asList(rawValues));
+				String[] rawValues = trimRawMAFLine.split(",");				
+				//System.out.println("RV="+Arrays.asList(rawValues));
+				
+				JsonArray arr;
+				JsonObject record;
+				int i=0;
+				for(String val : rawValues) {
+					arr = new JsonArray();
+					try{
+						Double score = new Double(val);
+						//String tvalue = BigDecimal.valueOf(score.doubleValue() / 100).toPlainString();
+						BigDecimal bd = new BigDecimal(score);
+						BigDecimal newVal = bd.divide(new BigDecimal(100), 6, RoundingMode.HALF_EVEN);
+						//arr.add(new JsonPrimitive(Double.parseDouble(tvalue)));
+						arr.add(new JsonPrimitive(newVal));
+						
+					} catch(NumberFormatException nfe) {
+						arr.add(new JsonPrimitive(val));
+					}			
+					
+					record = new JsonObject();					
+					record.add("_maf", arr);
+					
+					proot.add(MAF_LITERALS[i], record);
+					i++;					
+				}				
 			}
 			
-			//add mapValues as string to "processedInfo" and resturn it.
-						
-			return processedInfo;
+			return proot;
 		}
-		
-	}
-
+	}			
 }
