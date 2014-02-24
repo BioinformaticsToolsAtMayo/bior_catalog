ad4689bdccaa93753b5415ebffeb07a6dca6ce3d
m054457 <m054457@8f329c14-d232-4f17-8684-7ee34322b8dc>
2014-02-24 21:28:13 +0000

    publish_NcbiGene.sh : Create indexes on "gene", "GeneID", "HGNC".  Check for tabix and bgzip before processing.  Check if $BIOR_LITE_HOME set

--- publishers/publish_NcbiGene.sh
+++ publishers/publish_NcbiGene.sh
@@ -3,6 +3,7 @@
 # Assumptions:
 # 1) Bgzip is installed on the local machine and in $PATH
 # 2) Tabix is installed on the local machine and in $PATH
+# 3) The $BIOR_LITE_HOME variable is set, and the bin subdirectory is in your $PATH
 #------------------------------------------------------------------------------------------
 # Print each line that is executed (-x), and exit if any command fails (-e)
 #set -x -e
@@ -21,6 +22,26 @@ source setupEnv.sh
 cd $currentDir
 
 #------------------------------------------------------------------------------------------
+# Exit with error if the $BIOR_LITE_HOME path is not set
+#------------------------------------------------------------------------------------------
+if [ -z "$BIOR_LITE_HOME" ] ; then
+  echo "BIOR_LITE_HOME variable not set.  Please set this and try again"
+  exit 1;
+fi
+
+#------------------------------------------------------------------------------------------
+# Exit with error if tabix or bgzip not found
+#------------------------------------------------------------------------------------------
+hash bgzip  2>/dev/null || { \
+  echo "bgzip not found.  Please add it to your path and try again"; \
+  exit 1; \
+}
+hash tabix  2>/dev/null || { \
+  echo "tabix not found.  Please add it to your path and try again"; \
+  exit 1; \
+}
+
+#------------------------------------------------------------------------------------------
 # Given raw data files (*.gbs.txt) build JSON data file (which contains position + JSON)
 #------------------------------------------------------------------------------------------
 rawDataDir=$1
@@ -30,8 +51,10 @@ if [ -z "$rawDataDir" ] || [ ! -d "$rawDataDir" ] ; then
   echo "No directory specified for RAW DATA (or directory does not exist): $rawDataDir"
   echo "Example:  /data5/bsi/refdata-new/ncbi_genome/human/downloaded/latest/2014_02_04/Assembled_chromosomes/gbs"
   echo "Where this directory would contain files (one per chromosome) like: hs_ref_GRCh37.p13_chr11.gbs.gz"
+  echo ""
+  echo "Usage: publish_NcbiGene.sh  <rawDataDir>  <catalogOutDir>"
   exit 1;
-fi  
+fi
 
 if [ -z "$targetCatalogDir" ] || [ ! -d "$targetCatalogDir" ] ; then
   echo "No directory specified for TARGET CATALOG (or directory does not exist): $targetCatalogDir"
@@ -65,10 +88,13 @@ echo "Remove temp files..."
 rm $targetCatalogDir/genes.tsv
 
 #------------------------------------------------------------------------------------------
-# Create index on Gene Id
+# Create index on gene name ("gene"), Gene Id ("GeneID"), and HGNC Id ("HGNC")
 # Ex: Index <bgzipDataFile> <delimiter> <keyColumn> <isKeyColumnInt> <bgzipIndexOut>
 #------------------------------------------------------------------------------------------
-##### java -cp $BIOR_CATALOG_HOME/conf:$BIOR_CATALOG_HOME/lib/*  edu.mayo.bior.indexer.cmd.BuildBgzipIndex  
+### java -cp $BIOR_CATALOG_HOME/conf:$BIOR_CATALOG_HOME/lib/*  edu.mayo.bior.indexer.cmd.BuildBgzipIndex  
+bior_index_catalog -d $targetCatalogDir/genes.tsv.bgz -p gene
+bior_index_catalog -d $targetCatalogDir/genes.tsv.bgz -p GeneID
+bior_index_catalog -d $targetCatalogDir/genes.tsv.bgz -p HGNC
 
 
 echo "DONE."
